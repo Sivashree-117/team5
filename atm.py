b9ea960d561ef15de00a3ccf6d2aeec2e7fd55aa
Sivashree-117 <sivashree.pl2023@vitstudent.ac.in>
2026-01-08 19:29:33 +0530

    atm2

--- atm.py
+++ atm.py
@@ -2,4 +2,7 @@
 name = input("Enter your name: ")
 balance = int(input("Enter your account balance: "))
 print("\nWelcome", name)
-
+print("Your current balance is:", balance)
+while True:
+    amount = int(input("\nEnter withdrawal amount (multiple of 100): "))
+    # Check multiple of 100
