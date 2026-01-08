ae2932d7f07fa465687261ce6393bc1a326d0347
Sivashree-117 <sivashree.pl2023@vitstudent.ac.in>
2026-01-08 19:31:05 +0530

    atm3

--- atm.py
+++ atm.py
@@ -6,3 +6,7 @@ print("Your current balance is:", balance)
 while True:
     amount = int(input("\nEnter withdrawal amount (multiple of 100): "))
     # Check multiple of 100
+if amount % 100 != 0:
+        print("Please enter the amount in multiples of 100")
+        continue
+    # Check sufficient balance
